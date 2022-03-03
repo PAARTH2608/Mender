a988d630d389be8393eeba72e40eb7c5ea164edb
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-03-04 01:01:17 +0530

    deployment issues

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,59 @@
 import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import styled from "styled-components";
 import { useSession } from "next-auth/react";
 import { Palette } from "../components/Palette";
 import Loader from "../components/Loader";
 import { motion } from "framer-motion";
 import Register from "./register";
-import Link from "next/link";
 import Social from "../components/socials/social";
+import Women from "../public/women.svg";
 
 export const MainDiv = styled.div`
   background-color: ${Palette.maindiv};
   height: 100vh;
   width: 100%;
   display: flex;
+  flex-direction: ${(props) => (props.something ? "column" : "row")};
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   position: relative;
+  padding: 2%;
 `;
 const Title = styled(motion.div)`
-  height: 10vh;
-  width: 20%;
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
   border-radius: 10px;
   background: #8dc1c8;
   box-shadow: 20px 20px 60px #78a4aa, -20px -20px 60px #a2dee6;
   padding: 2vh;
   font-size: 2em;
+  margin-bottom: 2vh;
 `;
-
+const Helper = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Col = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+`;
+const ImgDiv = styled.div`
+width: 40%;
+display: flex;
+justify-content: center;
+align-items: center;
+`;
 const dropIn = {
   hidden: {
     x: "-100vh",
@@ -41,7 +68,7 @@ const dropIn = {
       stiffness: 500,
       damping: 30,
     },
-  }
+  },
 };
 export default function Home() {
   const [showLoader, setLoader] = useState(true);
@@ -56,24 +83,49 @@ export default function Home() {
     <>
       {!session && <Register />}
       {session && (
-        <MainDiv>
+        <MainDiv something="column">
           {showLoader ? (
-            <Loader />
+            <Helper>
+              <Loader />
+            </Helper>
           ) : (
             <>
-            <Link href='/main' passHref>
-              <Title
-                variants={dropIn}
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-              >
-                Hello Fellas
-              </Title>
-            </Link>
+              <Row>
+                <Col>
+                  <Title
+                    variants={dropIn}
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    height="10vh"
+                    width="40%"
+                  >
+                    Hello Fellas
+                  </Title>
+                  <Title
+                    variants={dropIn}
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    height="40vh"
+                    width="100%"
+                  >
+                    Concept :<br></br>
+                    This website works on the concept of Natural Language
+                    Processing. We have used a pre-trained NLP model which takes
+                    a paragraph/article as input, reads it and tries to answer
+                    the question asked by the user based on that article.
+                  </Title>
+                </Col>
+                <ImgDiv>
+                  <Image src="/women.svg" alt="women" height={340} width={340} />
+                </ImgDiv>
+              </Row>
             </>
           )}
-          <Social />
+          <Helper>
+            <Social />
+          </Helper>
         </MainDiv>
       )}
     </>
